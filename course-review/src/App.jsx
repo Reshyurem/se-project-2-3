0260fa5871bf2740c41152399d886f0fe1eaaab8
Abhay Patil <abhay.patil@students.iiit.ac.in>
2024-04-07 16:22:29 +0530

    setup

---
+++ course-review/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+    return (
+        <div className='app'>
+            <h1>Vite + React</h1>
+        </div>
+    )
+}
+
+export default App
